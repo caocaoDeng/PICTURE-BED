efa441a717afc65f430d41af7035239147f3780a
Dengcaocao <dengcaocao.dcc@gmail.com>
2025-05-23 00:40:52 +0800

    配置图片地址

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
