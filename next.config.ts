706d8cd76c67cbf0d088153360a10c4ac36c0b94
Deng·caocao <dengcaocao.dcc@gmail.com>
2025-08-14 17:09:00 +0800

    构建设置

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // 静态导出
+  output: 'export',
+  basePath: '/pictureManager',
+  distDir: 'build',
   images: {
     unoptimized: true,
     remotePatterns: [
